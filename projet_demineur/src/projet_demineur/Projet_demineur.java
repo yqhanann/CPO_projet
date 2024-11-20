570dafabc2350460f2f3fa98e0efc96b02bb4397
Yohanann Andrianasolo <yohan@10.3.62.201>
2024-11-20 14:47:32 +0100

    Creatio d'un nouveau projet

---
+++ projet_demineur/src/projet_demineur/Projet_demineur.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package projet_demineur;
+
+/**
+ *
+ * @author yohan
+ */
+public class Projet_demineur {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
